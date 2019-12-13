96c2089e8d2f0b774c93c326737f11caaa7df874
Zach Weber <weberzt@gmail.com>
2019-12-13 00:52:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,11 +57,3 @@ def reduce_to_all_true(source_array)
   return true 
 end
 
-def reduce_to_any_true(source_array)
-  i = 0 
-  while i < source_array.length do 
-    return true if source_array[i]
-    i += 1 
-  end
-  return false
-end
